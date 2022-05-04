0bdc43a5d4b6942c2ac086a1c7e0e33c8fc47734
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-05-03 20:13:05 -0700

    :mortar_board: Added .env & .gitignore file to protect information

--- manager.py
+++ manager.py
@@ -1,11 +1,15 @@
+from dotenv import load_dotenv
+import os
 
-secret_pass = '123'  # Create a dotenv file later on to hide information
+load_dotenv('.env')
+
+# Create a dotenv file later on to hide information
+secret_pass = os.getenv('MANAGER_PASS_WORD')
 login_tries = 3
 
 while True:
     pass_word = input('Please Enter your Pass: ')
-
-    if pass_word != '123':
+    if pass_word != secret_pass:
         login_tries -= 1
         print(f'You have {login_tries} attempts left.')
         if login_tries == 0:
